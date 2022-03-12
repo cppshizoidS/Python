17c9bf2daa5e1ff30d424889e87004a07e740d8b
Cppshizoid <alex.krivoschecov@gmail.com>
2022-03-12 15:48:18 +0300

    Add files via upload

---
+++ Third lesson.py
@@ -0,0 +1,26 @@
+# string operation
+s = 'analysis'
+a = 'linear algebra'
+u = 'math sciences'
+
+# Concatenation
+sum = s + '\t' + a + '\t' + u
+
+print("Concatenation:")
+print(sum)
+
+# Multiplication
+x = 'programming'
+y = 'mathematics'
+
+impl = (x + y) * 4
+
+print("Multiplication:")
+print(impl)
+
+#"in" operator
+v = 'Creating a string'
+
+print("Does it contain (in) in string? :")
+print("in" in v)
+
