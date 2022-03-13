02a71236a3f76043a0664660d8de5035a02c2995
Cppshizoid <alex.krivoschecov@gmail.com>
2022-03-13 13:01:35 +0300

    add bubble sort algorithm

--- Sort_Algorithms/Bubble_Sort.py
+++ Sort_Algorithms/Bubble_Sort.py
@@ -1,4 +1,3 @@
-# sorting algorithms of array
 def bubblesort(arr):
         n = len(arr)
 
@@ -8,38 +7,6 @@ def bubblesort(arr):
 
                 if arr[j] > arr[j + 1]:
                     arr[j], arr[j + 1] = arr[j + 1], arr[j]
-
-def countingSort(arr, min, max):
-    # initialize 0 array for keeping count of each key
-    count = [0] * (max + 1 - min)
-
-    # count the occurances of each number
-    for i in arr:
-        count[i - min] += 1
-
-    z = 0
-	# insert each element as many times onto the output array as they appear in count
-    for i in range(min, max + 1):
-        while (count[i - min] > 0):
-            print(i)
-            arr[z] = i
-            z += 1
-            count[i - min] -= 1
-
-    return arr
-
-def insertionSort(arr):
-    for i in range(1, len(arr)):
-        key = arr[i]
-        j = i - 1
-
-        while (j >= 0 and arr[j] > key):
-            arr[j + 1] = arr[j]
-            j = j - 1
-
-        arr[j + 1] = key
-
-def mergeSort(arr):
 
 
 arr = [20, 2, 37, 45, 6, 11, 219, 27]
