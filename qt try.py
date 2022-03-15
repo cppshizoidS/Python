822893433797f894d9cde8793848e69ad005225e
cppshizoidS <alex.krivoschecov@gmail.com>
2022-03-15 13:22:37 +0700

    qt try

--- qt try.py
+++ qt try.py
@@ -1,36 +1,38 @@
-import sys
-
 from PyQt5.QtWidgets import *
 from PyQt5.QtWidgets import QApplication, QMainWindow
+import sys
 
-def dispaly():
+
+def display():
     print(line_edit.text())
+
 
 def quit_window():
     window.close()
 
+
 app = QApplication(sys.argv)
 window = QMainWindow()
 window.setGeometry(500, 500, 300, 300)
-window.setWindowTitle("ABOBUS")
+window.setWindowTitle("CodersLegacy")
 
 label = QLabel(window)
-label.setText("PYQt 5 GUI Application")
+label.setText("PyQt5 GUI Application")
 label.adjustSize()
-label.move(90,30)
+label.move(90, 30)
 
-line_edit =QLineEdit(window)
-line_edit.move(100,70)
+line_edit = QLineEdit(window)
+line_edit.move(100, 70)
 
 button = QPushButton(window)
 button.setText("Print")
-button.clicked.connect(dispaly)
-button.move(100,130)
+button.clicked.connect(display)
+button.move(100, 130)
 
-button = QPushButton(window)
-button.setText("Quit")
-button.clicked.connect(quit_window())
-button.move(100,170)
+button2 = QPushButton(window)
+button2.setText("Quit")
+button2.clicked.connect(quit_window)
+button2.move(100, 170)
 
 window.show()
-sys.exit(app.exec())
+sys.exit(app.exec_())
