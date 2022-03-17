5a3fd36c7ac43a71c3bf1dae38bb56d3806d1da1
Cppshizoid <alex.krivoschecov@gmail.com>
2022-03-18 01:18:59 +0700

    Create Snake.py

---
+++ PyGame/Snake/Snake.py
@@ -0,0 +1,11 @@
+import pygame
+import sys
+import random
+import time
+
+
+class Game():
+    def __init__(self):
+        # задаем размеры экрана
+        self.screen_width = 720
+        self.screen_height = 460
