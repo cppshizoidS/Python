f3288c19c697009baab3f041aa1fd282b3366525
cppshizoidS <alex.krivoschecov@gmail.com>
2022-03-17 14:04:48 +0700

    realese

--- Tkinter/Sapper/Sapper.py
+++ Tkinter/Sapper/Sapper.py
@@ -1,242 +1,234 @@
-import tkinter as tk
-from random import shuffle
-from tkinter.messagebox import showinfo, showerror
+import tkinter, configparser, random, os, tkinter.messagebox, tkinter.simpledialog
 
-colors = {
-        0: 'white',
-        1: 'blue',
-        2: 'green',
-        3: 'yellow',
-        4: '#93edab',
-        5: '#fee8b9',
-        6: '#2f0801',
-        7: '#1b8ab7',
-        8: '#77d0f1'
-    }
+window = tkinter.Tk()
 
+window.title("Minesweeper")
 
-class MyButton(tk.Button):
+#prepare default values
 
-        def __init__(self, master, x, y, number=0, *args, **kwargs):
-            super(MyButton, self).__init__(master, width=3, font='Arial 15 bold', *args, **kwargs)
-            self.x = x
-            self.y = y
-            self.number = number
-            self.is_mine = False
-            self.count_bomb = 0
-            self.is_open = False
+rows = 10
+cols = 10
+mines = 10
+
+field = []
+buttons = []
+
+colors = ['#FFFFFF', '#0000FF', '#008200', '#FF0000', '#000084', '#840000', '#008284', '#840084', '#000000']
+
+gameover = False
+customsizes = []
 
 
-        def __repr__(self):
-            return f'MyButton{self.x} {self.y} {self.number} {self.is_mine}'    #найди ошибку
+def createMenu():
+    menubar = tkinter.Menu(window)
+    menusize = tkinter.Menu(window, tearoff=0)
+    menusize.add_command(label="small (10x10 with 10 mines)", command=lambda: setSize(10, 10, 10))
+    menusize.add_command(label="medium (20x20 with 40 mines)", command=lambda: setSize(20, 20, 40))
+    menusize.add_command(label="big (35x35 with 120 mines)", command=lambda: setSize(35, 35, 120))
+    menusize.add_command(label="custom", command=setCustomSize)
+    menusize.add_separator()
+    for x in range(0, len(customsizes)):
+        menusize.add_command(label=str(customsizes[x][0])+"x"+str(customsizes[x][1])+" with "+str(customsizes[x][2])+" mines", command=lambda customsizes=customsizes: setSize(customsizes[x][0], customsizes[x][1], customsizes[x][2]))
+    menubar.add_cascade(label="size", menu=menusize)
+    menubar.add_command(label="exit", command=lambda: window.destroy())
+    window.config(menu=menubar)
 
 
-    class Sapper:
-        window = tk.Tk()
-        ROW = 10
-        COLUMNS = 7
-        MINES = 8
-        IS_GAME_OVER = False
-        IS_FIRST_CLICK = True
+def setCustomSize():
+    global customsizes
+    r = tkinter.simpledialog.askinteger("Custom size", "Enter amount of rows")
+    c = tkinter.simpledialog.askinteger("Custom size", "Enter amount of columns")
+    m = tkinter.simpledialog.askinteger("Custom size", "Enter amount of mines")
+    while m > r*c:
+        m = tkinter.simpledialog.askinteger("Custom size", "Maximum mines for this dimension is: " + str(r*c) + "\nEnter amount of mines")
+    customsizes.insert(0, (r,c,m))
+    customsizes = customsizes[0:5]
+    setSize(r,c,m)
+    createMenu()
 
-        def __init__(self):
-            self.buttons = []
-            for i in range(Sapper.ROW + 2):
-                temp = []
-                for j in range(Sapper.COLUMNS + 2):
-                    btn = MyButton(Sapper.window, x=i, y=j)
-                    btn.config(command=lambda button=btn: self.click(button))
-                    btn.bind("<Button-3>", self.right_click)
-                    temp.append(btn)
-                self.buttons.append(temp)
+def setSize(r,c,m):
+    global rows, cols, mines
+    rows = r
+    cols = c
+    mines = m
+    saveConfig()
+    restartGame()
 
-        def right_click(self, event):
-            if Sapper.IS_GAME_OVER:
-                return
-            cur_btn = event.windget
-            if cur_btn['state'] == 'normal':
-                cur_btn['state'] == 'disabled'
-                cur_btn['text'] ='🚩'
-                cur_btn['disabledforground'] = 'red'
-            elif cur_btn['text'] == '🚩':
-                cur_btn['text'] = ' '
-                cur_btn['state'] = 'normal'
+def saveConfig():
+    global rows, cols, mines
+    #configuration
+    config = configparser.SafeConfigParser()
+    config.add_section("game")
+    config.set("game", "rows", str(rows))
+    config.set("game", "cols", str(cols))
+    config.set("game", "mines", str(mines))
+    config.add_section("sizes")
+    config.set("sizes", "amount", str(min(5,len(customsizes))))
+    for x in range(0,min(5,len(customsizes))):
+        config.set("sizes", "row"+str(x), str(customsizes[x][0]))
+        config.set("sizes", "cols"+str(x), str(customsizes[x][1]))
+        config.set("sizes", "mines"+str(x), str(customsizes[x][2]))
 
+    with open("config.ini", "w") as file:
+        config.write(file)
 
-        def click(self, clicked_button: MyButton):
-            if Sapper.IS_GAME_OVER:
-                return None
+def loadConfig():
+    global rows, cols, mines, customsizes
+    config = configparser.SafeConfigParser()
+    config.read("config.ini")
+    rows = config.getint("game", "rows")
+    cols = config.getint("game", "cols")
+    mines = config.getint("game", "mines")
+    amountofsizes = config.getint("sizes", "amount")
+    for x in range(0, amountofsizes):
+        customsizes.append((config.getint("sizes", "row"+str(x)), config.getint("sizes", "cols"+str(x)), config.getint("sizes", "mines"+str(x))))
 
-            if Sapper.IS_FIRST_CLICK:
-                self.insert_mines(clicked_button.number)
-                self.count_mines_in_buttons()
-                self.print_buttons()
-                Sapper.IS_FIRST_CLICK = False
+def prepareGame():
+    global rows, cols, mines, field
+    field = []
+    for x in range(0, rows):
+        field.append([])
+        for y in range(0, cols):
+            #add button and init value for game
+            field[x].append(0)
+    #generate mines
+    for _ in range(0, mines):
+        x = random.randint(0, rows-1)
+        y = random.randint(0, cols-1)
+        #prevent spawning mine on top of each other
+        while field[x][y] == -1:
+            x = random.randint(0, rows-1)
+            y = random.randint(0, cols-1)
+        field[x][y] = -1
+        if x != 0:
+            if y != 0:
+                if field[x-1][y-1] != -1:
+                    field[x-1][y-1] = int(field[x-1][y-1]) + 1
+            if field[x-1][y] != -1:
+                field[x-1][y] = int(field[x-1][y]) + 1
+            if y != cols-1:
+                if field[x-1][y+1] != -1:
+                    field[x-1][y+1] = int(field[x-1][y+1]) + 1
+        if y != 0:
+            if field[x][y-1] != -1:
+                field[x][y-1] = int(field[x][y-1]) + 1
+        if y != cols-1:
+            if field[x][y+1] != -1:
+                field[x][y+1] = int(field[x][y+1]) + 1
+        if x != rows-1:
+            if y != 0:
+                if field[x+1][y-1] != -1:
+                    field[x+1][y-1] = int(field[x+1][y-1]) + 1
+            if field[x+1][y] != -1:
+                field[x+1][y] = int(field[x+1][y]) + 1
+            if y != cols-1:
+                if field[x+1][y+1] != -1:
+                    field[x+1][y+1] = int(field[x+1][y+1]) + 1
 
-            if clicked_button.is_mine:
-                clicked_button.config(text="*", background="red", disabledforeground='black')
-                clicked_button.is_open = True
-                Sapper.IS_GAME_OVER = True
-                showinfo('Game Over', 'You lose!')
-                for i in range(1, Sapper.ROW + 1):
-                    for j in range(1, Sapper.COLUMNS + 1):
-                        btn = self.buttons[i][j]
-                        if btn.is_mine:
-                            btn['text'] = '*'
+def prepareWindow():
+    global rows, cols, buttons
+    tkinter.Button(window, text="Restart", command=restartGame).grid(row=0, column=0, columnspan=cols, sticky=tkinter.N+tkinter.W+tkinter.S+tkinter.E)
+    buttons = []
+    for x in range(0, rows):
+        buttons.append([])
+        for y in range(0, cols):
+            b = tkinter.Button(window, text=" ", width=2, command=lambda x=x,y=y: clickOn(x,y))
+            b.bind("<Button-3>", lambda e, x=x, y=y:onRightClick(x, y))
+            b.grid(row=x+1, column=y, sticky=tkinter.N+tkinter.W+tkinter.S+tkinter.E)
+            buttons[x].append(b)
 
-            else:
-                color = colors.get(clicked_button.count_bomb, 'black')
-                if clicked_button.count_bomb:
-                    clicked_button.config(text=clicked_button.count_bomb, disabledforeground=color)
-                    clicked_button.is_open = True
-                else:
-                    self.breadth_first_search(clicked_button)
-            clicked_button.config(state='disabled')
-            clicked_button.config(relief=tk.SUNKEN)
+def restartGame():
+    global gameover
+    gameover = False
+    #destroy all - prevent memory leak
+    for x in window.winfo_children():
+        if type(x) != tkinter.Menu:
+            x.destroy()
+    prepareWindow()
+    prepareGame()
 
-        def breadth_first_search(self, btn: MyButton):  #обход в ширину
-            queue = [btn]
-            while queue:
-                cur_btn = queue.pop()
-                color = colors.get(cur_btn.count_bomb, 'black')
-                if cur_btn.count_bomb:
-                    cur_btn.config(text=cur_btn.count_bomb, disabledforeground=color)
-                else:
-                    cur_btn.config(text='', disabledforeground=color)
-                cur_btn.is_open = True
-                cur_btn.config(state='disabled')
-                cur_btn.config(relief=tk.SUNKEN)
+def clickOn(x,y):
+    global field, buttons, colors, gameover, rows, cols
+    if gameover:
+        return
+    buttons[x][y]["text"] = str(field[x][y])
+    if field[x][y] == -1:
+        buttons[x][y]["text"] = "*"
+        buttons[x][y].config(background='red', disabledforeground='black')
+        gameover = True
+        tkinter.messagebox.showinfo("Game Over", "You have lost.")
+        #now show all other mines
+        for _x in range(0, rows):
+            for _y in range(cols):
+                if field[_x][_y] == -1:
+                    buttons[_x][_y]["text"] = "*"
+    else:
+        buttons[x][y].config(disabledforeground=colors[field[x][y]])
+    if field[x][y] == 0:
+        buttons[x][y]["text"] = " "
+        #now repeat for all buttons nearby which are 0... kek
+        autoClickOn(x,y)
+    buttons[x][y]['state'] = 'disabled'
+    buttons[x][y].config(relief=tkinter.SUNKEN)
+    checkWin()
 
-                if cur_btn.count_bomb == 0:
-                    x,y = cur_btn.x, cur_btn.y
-                    for dx in [-1, 0, 1]:
-                        for dy in [-1, 0, 1]:
-                            #if not abs(dx - dy) == 1:
-                            #   continue
+def autoClickOn(x,y):
+    global field, buttons, colors, rows, cols
+    if buttons[x][y]["state"] == "disabled":
+        return
+    if field[x][y] != 0:
+        buttons[x][y]["text"] = str(field[x][y])
+    else:
+        buttons[x][y]["text"] = " "
+    buttons[x][y].config(disabledforeground=colors[field[x][y]])
+    buttons[x][y].config(relief=tkinter.SUNKEN)
+    buttons[x][y]['state'] = 'disabled'
+    if field[x][y] == 0:
+        if x != 0 and y != 0:
+            autoClickOn(x-1,y-1)
+        if x != 0:
+            autoClickOn(x-1,y)
+        if x != 0 and y != cols-1:
+            autoClickOn(x-1,y+1)
+        if y != 0:
+            autoClickOn(x,y-1)
+        if y != cols-1:
+            autoClickOn(x,y+1)
+        if x != rows-1 and y != 0:
+            autoClickOn(x+1,y-1)
+        if x != rows-1:
+            autoClickOn(x+1,y)
+        if x != rows-1 and y != cols-1:
+            autoClickOn(x+1,y+1)
 
-                            next_btn = self.buttons[x+dx][y+dy]
-                            if not next_btn.is_open and 1<=next_btn.x<=Sapper.ROW and \
-                                1 <= next_btn.y <= Sapper.COLUMNS and next_btn not in queue:
-                                queue.append(next_btn)
+def onRightClick(x,y):
+    global buttons
+    if gameover:
+        return
+    if buttons[x][y]["text"] == "?":
+        buttons[x][y]["text"] = " "
+        buttons[x][y]["state"] = "normal"
+    elif buttons[x][y]["text"] == " " and buttons[x][y]["state"] == "normal":
+        buttons[x][y]["text"] = "?"
+        buttons[x][y]["state"] = "disabled"
 
-        def reload(self):
-            [child.destroy() for child  in self.window.winfo_children()]
-            self.__init__()
-            self.create_widgets()
-            Sapper.IS_FIRST_CLICK = True
-            Sapper.IS_GAME_OVER = False
+def checkWin():
+    global buttons, field, rows, cols
+    win = True
+    for x in range(0, rows):
+        for y in range(0, cols):
+            if field[x][y] != -1 and buttons[x][y]["state"] == "normal":
+                win = False
+    if win:
+        tkinter.messagebox.showinfo("Gave Over", "You have won.")
 
-        def create_settings_win(self):
-            win_settings = tk.Toplevel(self.window)
-            win_settings.wm_title('Settings')
-            tk.Label(win_settings, text='Numer of strings').grid(row=0, column=1)
-            row_entry = tk.Entry(win_settings)
-            row_entry.insert(0, Sapper.ROW)
-            row_entry.grid(row=0, column= 1, padx= 20, pady= 20)
-            tk.Label(win_settings, text='Numer of columns').grid(row=1, column=1)
-            column_entry = tk.Entry(win_settings)
-            row_entry.insert(0, Sapper.COLUMNS)
-            column_entry.grid(row=1, column= 1, padx= 20, pady= 20)
-            tk.Label(win_settings, text='Numer of mines').grid(row=2, column=1)
-            mines_entry = tk.Entry(win_settings)
-            row_entry.insert(0, Sapper.MINES)
-            mines_entry.grid(row=2, column= 1, padx= 20, pady= 20)
-            save_btn = tk.Button(win_settings, text = 'Apply',
-                      command=lambda :self.change_settings(row_entry, column_entry, mines_entry))
-            save_btn.grid(row=3, column=0, columnspan=2,padx= 20, pady= 20)
+if os.path.exists("config.ini"):
+    loadConfig()
+else:
+    saveConfig()
 
-        def change_settings(self, row : tk.Entry, column: tk.Entry, mines: tk.Entry):
-            try:
-                int(row.get()), int(column.get()), int(mines.get())
-            except ValueError:
-                showerror('You write an wrong value!')
-            Sapper.ROW = int(row.get())
-            Sapper.COLUMNS = int(column.get())
-            Sapper.MINES = int(mines.get())
-            self.reload()
+createMenu()
 
-        def create_widgets(self):
-
-            menubar = tk.Menu(self.window)
-            self.window.config(menu=menubar)
-
-            settings_menu = tk.Menu(menubar, tearoff=0)
-            settings_menu.add_command(label='Play', command=self.reload)
-            settings_menu.add_command(label='Settings', command=self.create_settings_win)
-            settings_menu.add_command(label='Exit', command=self.window.destroy)
-            menubar.add_cascade(label='File', menu=settings_menu)
-
-
-            count = 1
-            for i in range(1, Sapper.ROW + 1):
-                for j in range(1, Sapper.COLUMNS + 1):
-                    btn = self.buttons[i][j]
-                    btn.number = count
-                    btn.grid(row=i, column=j, stick='NWES')
-                    count += 1
-
-            for i in range(1, Sapper.ROW + 1):
-                tk.Grid.rowconfigure(self.window, i, weight = 1)
-                
-            for i in range(1 , Sapper.COLUMNS + 1):
-                tk.Grid.columnconfigure(self.window, i, weight = 1)
-
-        def open_all_buttons(self):
-            for i in range(Sapper.ROW + 2):
-                for j in range(Sapper.COLUMNS + 2):
-                    btn = self.buttons[i][j]
-                    if btn.is_mine:
-                        btn.config(text="*", background="red", disabledforeground='black')
-                    elif btn.count_bomb in colors:
-                        color = colors.get(btn.count_bomb, 'black')
-                        btn.config(text=btn.count_bomb, fg=color)
-
-        def start(self):
-            self.create_widgets()
-            #self.open_all_buttons()
-            Sapper.window.mainloop()
-
-        def print_buttons(self):
-            for i in range(1, Sapper.ROW + 1):
-                for j in range(1, Sapper.COLUMNS + 1):
-                    btn = self.buttons[i][j]
-                    if btn.is_mine:
-                        print('B', end='')
-                    else:
-                        print(btn.count_bomb, end='')
-                print()
-
-        def insert_mines(self, number: int):
-            index_mines = self.get_mines_places(number)
-            print(index_mines)
-            for i in range(1, Sapper.ROW + 1):
-                for j in range(1, Sapper.COLUMNS + 1):
-                    btn = self.buttons[i][j]
-                    if btn.number in index_mines:
-                        btn.is_mine = True
-
-
-        def count_mines_in_buttons(self):
-            for i in range(1, Sapper.ROW + 1):
-                for j in range(1, Sapper.COLUMNS + 1):
-                    btn = self.buttons[i][j]
-                    count_bomb = 0
-                    if not btn.is_mine:
-                        for row_dx in [-1, 0, 1]:
-                            for col_dx in [-1, 0, 1]:
-                                neighbour = self.buttons[i + row_dx][j + col_dx]
-                                if neighbour.is_mine:
-                                    count_bomb += 1
-                    btn.count_bomb = count_bomb
-
-        @staticmethod
-        def get_mines_places(exclude_number: int):
-            indexes = list(range(1, Sapper.COLUMNS * Sapper.ROW + 1))
-            print(f'Remove button number {exclude_number}')
-            indexes.remove(exclude_number)
-            shuffle(indexes)
-            return indexes[:Sapper.MINES]
-
-
-game = Sapper()
-game.start()
+prepareWindow()
+prepareGame()
+window.mainloop()
