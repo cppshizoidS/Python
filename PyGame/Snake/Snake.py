5f7e2ab4ecbf1e5f848c0fb1eb4a74ff5d3a31a4
Cppshizoid <alex.krivoschecov@gmail.com>
2022-03-18 01:37:29 +0700

    Update Snake.py

--- PyGame/Snake/Snake.py
+++ PyGame/Snake/Snake.py
@@ -9,3 +9,18 @@ class Game():
         # задаем размеры экрана
         self.screen_width = 720
         self.screen_height = 460
+
+    # необходимые цвета
+        self.red = pygame.Color(255, 0, 0)
+        self.green = pygame.Color(0, 255, 0)
+        self.black = pygame.Color(0, 0, 0)
+        self.white = pygame.Color(255, 255, 255)
+        self.brown = pygame.Color(165, 42, 42)
+
+        # Frame per second controller
+        # будет задавать количество кадров в секунду
+        self.fps_controller = pygame.time.Clock()
+
+        # переменная для оторбражения результата
+        # (сколько еды съели)
+        self.score = 0
