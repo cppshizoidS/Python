3ad2c56f85d4d0be849d81336c682853aee79e0e
Cppshizoid <alex.krivoschecov@gmail.com>
2022-03-18 01:49:44 +0700

    Update Snake.py
    surface and check errors

--- PyGame/Snake/Snake.py
+++ PyGame/Snake/Snake.py
@@ -24,3 +24,19 @@ class Game():
         # переменная для оторбражения результата
         # (сколько еды съели)
         self.score = 0
+        
+    def init_and_check_for_errors(self):
+        """Начальная функция для инициализации и
+           проверки как запустится pygame"""
+        check_errors = pygame.init()
+        if check_errors[1] > 0:
+            sys.exit()
+        else:
+            print('Ok')
+
+    def set_surface_and_title(self):
+        """Задаем surface(поверхность поверх которой будет все рисоваться)
+        и устанавливаем загаловок окна"""
+        self.play_surface = pygame.display.set_mode((
+            self.screen_width, self.screen_height))
+        pygame.display.set_caption('Snake Game')
