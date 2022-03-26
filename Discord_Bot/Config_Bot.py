76c17f7841c4f2fa095683b5a10cdb0a43220a5a
Cppshizoid <alex.krivoschecov@gmail.com>
2022-03-27 04:08:38 +0700

    Create Config_Bot.py

---
+++ Discord_Bot/Config_Bot.py
@@ -0,0 +1,15 @@
+TOKEN = ''  # bot token
+
+POST_ID = 957373806517833768  # post id reactions form
+
+# list of roles
+ROLES = {
+    '🔥': 865644901995774012,  # AMOGUS role
+    '🖥️': 865478556850978816,  # programmer role
+    '🔫': 957372173239074847   #CS:GO role
+}
+
+# exlude roles from couning
+EXCROLES = ()
+
+MAX_ROLES_PER_USER = 2  # max amount of roles for one user
