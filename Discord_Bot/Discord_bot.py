2941b9bcdc1ed41ad3e238259269b3956b66a321
Cppshizoid <alex.krivoschecov@gmail.com>
2022-03-27 03:28:47 +0700

    Update Discord_bot.py

--- Discord_Bot/Discord_bot.py
+++ Discord_Bot/Discord_bot.py
@@ -1,11 +1,21 @@
 import discord
+from discord import utils
+
+import config
 
 class MyClient(discord.Client):
     async def on_ready(self):
         print('Loffed on as  {0}!'.format(self.user))
 
-    async def on_message(self, message):
-        print('Message from {0.author}: {0, content}'. format(message))
+    async def on_raw_reactions_add(self, payload):
+        channel = self.get_channel(payload.channel_id)
+        pass
+
+    async def on_raw_reactions_remove(self, payload):
+        pass
+
 
 client = MyClient()
 client.run(#Token)
+
+client.run(comfig.TOKEN)
