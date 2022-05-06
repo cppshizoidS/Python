571f87cd8b097b2658ea6de0996ef63b689f3bd9
Cppshizoid <alex.krivoschecov@gmail.com>
2022-05-07 06:06:36 +0700

    Update on_message.py

--- TgBot-v2/src/commands/on_message.py
+++ TgBot-v2/src/commands/on_message.py
@@ -7,5 +7,4 @@ import utils.utils as utl
 
 def handle_message(update: Update, context: CallbackContext):
 
-    # TODO: YOUR MESSAGE HANDLING HERE
     utl.send_msg(update, context, "Hey!\nWhat can I do for you", kb.main_menu)
