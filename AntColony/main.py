833f01c0ad69097916c9450721eb2acab4e835ac
Cppshizoid <alex.krivoschecov@gmail.com>
2022-05-28 14:17:25 +0700

    Update main.py

--- AntColony/main.py
+++ AntColony/main.py
@@ -2,20 +2,16 @@ from grid import Grid
 from colony import Colony
 from application import Application
 
-# Creating the grid
+
 grid_map = Grid()
-# loading the grid from the file map.txt
+
 grid_map.load_grid("map.txt")
 
-# Create the Colony moving in grid_map
+
 ants_colony = Colony(grid_map)
 
-# Start the interface
 app = Application()
-# Start adding walls, if necessary
+
 app.begin_draw(grid_map)
 
-# Uncomment if you want to kepe the modified grid
-#grid_map.save_grid("map2.txt")
-# Start the colony
 app.start_app(grid_map, ants_colony)
